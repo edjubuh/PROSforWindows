b5289763eab307ce637b1daeb1cf916a03decadc
Elliot Berman <elliot@elliotjb.com>
2015-06-25 23:11:25 -0400

    Add initial theming, made changes to MahApps

---
+++ PROSforWindows/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MahApps.Metro.Controls;
+using PROSforWindows.ViewModels;
+using System.Windows;
+
+namespace PROSforWindows.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
