fe32972ec3b2eeab59a07e82d48195d35ceae9c7
Elliot Berman <elliot@elliotjb.com>
2015-06-27 16:34:46 -0400

    Layout and appearance updates for buttons

--- PROSforWindows/Helpers/DialogService.cs
+++ PROSforWindows/Helpers/DialogService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PROSforWindows.Helpers
+namespace PROSforWindows.Resources
 {
     // Based off of http://stackoverflow.com/questions/22083213/mahapps-messageboxes-using-mvvm
     public class DialogService
